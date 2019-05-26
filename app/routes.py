bd09935dab7576be09f9568bc5b0a2c0ca0adb73
Laurent Dumont <laurentfdumont@gmail.com>
2019-05-26 12:30:23 -0400

    Add alert screens and random logic

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,24 @@
+#Flask imports
 from app import app
-import requests
-import json
 from flask import render_template
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title='ALERT! SHIT IS ON FIRE!')
+#Application specific imports.
+import requests
+import json
+import random
 
-@app.route('/potato')
+@app.route('/')
 def get_prometheus():
+    alert_screens = ['alert_1.gif', 'alert_2.gif', 'alert_3.gif', 'alert_4.gif', 'alert_5.gif', 'alert_6.gif']
     data = requests.get('http://g.cmaker.studio:9090/api/v1/alerts')
     json_data = json.loads(data.content)
     number_of_alerts = len(json_data['data']['alerts'])
     if number_of_alerts > 0:
-      return render_template('NOT-OK.html')
+        len(alert_screens)
+        random_int = random.randint(0,5)
+        print(random_int)
+        alert_screen = alert_screens[random_int]
+        return render_template('NOT-OK.html', alert_screen=alert_screen, title='ALERT! SHIT IS ON FIRE!')
     else:
-      return render_template('OK.html')
+        return render_template('OK.html', title='THINGS ARE FINE')
   
